a10423a4572e69ca82533e0364067adcb9d9d4c3
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-11-22 15:25:56 +0100

    Created vite structure

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <AppHeader></AppHeader>
+  <AppMain/>
+  <AppFooter/>
+</template>
+
+<script>
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+  export default {
+    components: {
+      AppHeader,
+      AppMain,
+      AppFooter
+    },
+  }
+</script>
+<style lang="scss">
+    @use './style/reset.scss'
+</style>
